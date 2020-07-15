d0bfcc3c3df88f3e84dfe00d4b09bb2db42163c9
Huaqi Fang <578567190@qq.com>
2020-07-15 16:38:20 +0800

    application: update demo_plic example using hbird
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- application/baremetal/demo_plic/main.c
+++ application/baremetal/demo_plic/main.c
@@ -1,39 +1,55 @@
 // See LICENSE for license details.
 #include <stdio.h>
-#include "hbird_sdk_soc.h"
+#include "hbird_sdk_hal.h"
 
+#define BTN1_GPIO_OFS           30
+#define BTN2_GPIO_OFS           31
+#define PLIC_BTN1_INT           (8+BTN1_GPIO_OFS)
+#define PLIC_BTN2_INT           (8+BTN2_GPIO_OFS)
 
-#define HIGHER_INTLEVEL         2
-#define LOWER_INTLEVEL          1
-
-
-// plic int0 interrupt handler
-void plic_int0_handler(void)
+// plic btn1 interrupt handler
+void plic_btn1_handler(void)
 {
-
+    printf("Enter Button 1 interrupt\n");
+    gpio_clear_interrupt(GPIO, SOC_BUTTON_1_GPIO_OFS, GPIO_INT_RISE);
+    gpio_toggle(GPIO, SOC_LED_BLUE_GPIO_MASK);
 }
 
-// plic int1 interrupt handler
-void plic_int1_handler(void)
+// plic btn2 interrupt handler
+void plic_btn2_handler(void)
 {
+    printf("Enter Button 2 interrupt\n");
+    gpio_clear_interrupt(GPIO, SOC_BUTTON_2_GPIO_OFS, GPIO_INT_RISE);
+    gpio_toggle(GPIO, SOC_LED_GREEN_GPIO_MASK);
+}
 
+void board_gpio_init(void)
+{
+    gpio_enable_input(GPIO, SOC_BUTTON_GPIO_MASK);
+    gpio_set_pue(GPIO, SOC_BUTTON_GPIO_MASK, GPIO_BIT_ALL_ONE);
+
+    gpio_enable_output(GPIO, SOC_LED_GPIO_MASK);
+    gpio_write(GPIO, SOC_LED_GPIO_MASK, GPIO_BIT_ALL_ZERO);
+    // Initialize the button as rising interrupt enabled
+    gpio_enable_interrupt(GPIO, SOC_BUTTON_GPIO_MASK, GPIO_INT_RISE);
 }
 
 int main(int argc, char **argv)
 {
     int32_t returnCode;
 
+    board_gpio_init();
+
     // inital plic int0 interrupt
-    returnCode = PLIC_Register_IRQ(PLIC_INT0_IRQn, 1, plic_int0_handler);
+    returnCode = PLIC_Register_IRQ(PLIC_BTN1_INT, 1, plic_btn1_handler);
 
     // inital plic int1 interrupt
-    returnCode = PLIC_Register_IRQ(PLIC_INT1_IRQn, 1, plic_int1_handler);
+    returnCode = PLIC_Register_IRQ(PLIC_BTN2_INT, 2, plic_btn2_handler);
 
     // Enable interrupts in general.
     __enable_irq();
 
-    // Wait for timer interrupt and software interrupt
-    // triggered periodly
+    // Wait button 1 and button 2 pressed to trigger interrupt
     while (1);
     return 0;
 }
