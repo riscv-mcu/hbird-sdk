61b01a8bd73f5cec9275538f8c1515efdd273b11
canhu <hucan0216@163.com>
2020-08-17 15:16:03 +0800

    SoC: add UART parity & interrupt & stopbit set function for hbirdv2

--- SoC/hbirdv2/Common/Include/hbirdv2_uart.h
+++ SoC/hbirdv2/Common/Include/hbirdv2_uart.h
@@ -12,16 +12,28 @@ typedef enum uart_stop_bit {
     UART_STOP_BIT_2 = 1
 } UART_STOP_BIT;
 
+typedef enum uart_parity_bit {
+    UART_ODD   = 0,
+    UART_EVEN  = 1,
+    UART_SPACE = 2,
+    UART_MARK  = 3
+} UART_PARITY_BIT;
+
 int32_t uart_init(UART_TypeDef *uart, uint32_t baudrate);
 int32_t uart_config_stopbit(UART_TypeDef *uart, UART_STOP_BIT stopbit);
+int32_t uart_enable_paritybit(UART_TypeDef *uart);
+int32_t uart_disable_paritybit(UART_TypeDef *uart);
+int32_t uart_set_parity(UART_TypeDef *uart, UART_PARITY_BIT paritybit);
 int32_t uart_write(UART_TypeDef *uart, uint8_t val);
 uint8_t uart_read(UART_TypeDef *uart);
-int32_t uart_set_tx_watermark(UART_TypeDef *uart, uint32_t watermark);
-int32_t uart_enable_txint(UART_TypeDef *uart);
-int32_t uart_disable_txint(UART_TypeDef *uart);
-int32_t uart_set_rx_watermark(UART_TypeDef *uart, uint32_t watermark);
-int32_t uart_enable_rxint(UART_TypeDef *uart);
-int32_t uart_disable_rxint(UART_TypeDef *uart);
+int32_t uart_enable_tx_empt_int(UART_TypeDef *uart);
+int32_t uart_disable_tx_empt_int(UART_TypeDef *uart);
+int32_t uart_set_rx_th(UART_TypeDef *uart, uint8_t th);
+int32_t uart_enable_rx_th_int(UART_TypeDef *uart);
+int32_t uart_disable_rx_th_int(UART_TypeDef *uart);
+int32_t uart_enable_rx_err_int(UART_TypeDef *uart);
+int32_t uart_disable_rx_err_int(UART_TypeDef *uart);
+int32_t uart_get_int_status(UART_TypeDef *uart);
 
 #ifdef __cplusplus
 }
--- SoC/hbirdv2/Common/Source/Drivers/hbirdv2_uart.c
+++ SoC/hbirdv2/Common/Source/Drivers/hbirdv2_uart.c
@@ -8,10 +8,10 @@ int32_t uart_init(UART_TypeDef *uart, uint32_t baudrate)
     }
 
     unsigned int uart_div = SystemCoreClock / baudrate - 1;
-    uart->LCR = 0x83;
+    uart->LCR = 0x80;
     uart->DLM = (uart_div >> 8) & 0xFF;
     uart->DLL = uart_div        & 0xFF;
-    uart->FCR = 0xA7;
+    uart->FCR = 0xC6;
     uart->LCR = 0x03;
 
     return 0;
@@ -22,6 +22,44 @@ int32_t uart_config_stopbit(UART_TypeDef *uart, UART_STOP_BIT stopbit)
     if (__RARELY(uart == NULL)) {
         return -1;
     }
+
+    uart->LCR &= 0xFFFFFFFB;
+    uart->LCR |= (stopbit << 2);
+
+    return 0;
+}
+
+int32_t uart_enable_paritybit(UART_TypeDef *uart)
+{
+    if (__RARELY(uart == NULL)) {
+        return -1;
+    }
+
+    uart->LCR |= 0x8;
+
+    return 0;
+}
+
+int32_t uart_disable_paritybit(UART_TypeDef *uart)
+{
+    if (__RARELY(uart == NULL)) {
+        return -1;
+    }
+
+    uart->LCR &= 0xFFFFFFF7;
+
+    return 0;
+}
+
+int32_t uart_set_parity(UART_TypeDef *uart, UART_PARITY_BIT paritybit)
+{
+    if (__RARELY(uart == NULL)) {
+        return -1;
+    }
+
+    uart->LCR &= 0xFFFFFFCF;
+    uart->LCR |= (paritybit << 4);
+
     return 0;
 }
 
@@ -50,50 +88,88 @@ uint8_t uart_read(UART_TypeDef *uart)
     return (uint8_t)(reg & 0xFF);
 }
 
-int32_t uart_set_tx_watermark(UART_TypeDef *uart, uint32_t watermark)
+
+int32_t uart_enable_tx_empt_int(UART_TypeDef *uart)
 {
     if (__RARELY(uart == NULL)) {
         return -1;
     }
+
+    uart->IER |= 0x2;
     return 0;
 }
 
-int32_t uart_enable_txint(UART_TypeDef *uart)
+int32_t uart_disable_tx_empt_int(UART_TypeDef *uart)
 {
     if (__RARELY(uart == NULL)) {
         return -1;
     }
+
+    uart->IER &= 0xFFFFFFFD;
     return 0;
 }
 
-int32_t uart_disable_txint(UART_TypeDef *uart)
+int32_t uart_set_rx_th(UART_TypeDef *uart, uint8_t th)
 {
     if (__RARELY(uart == NULL)) {
         return -1;
     }
+
+    if(th > 3) {
+       th = 3;
+    }
+
+    uart->FCR &= 0xFFFFFF3F;
+    uart->FCR |= (th << 6);
     return 0;
 }
 
-int32_t uart_set_rx_watermark(UART_TypeDef *uart, uint32_t watermark)
+int32_t uart_enable_rx_th_int(UART_TypeDef *uart)
 {
     if (__RARELY(uart == NULL)) {
         return -1;
     }
+
+    uart->IER |= 0x1;
     return 0;
 }
 
-int32_t uart_enable_rxint(UART_TypeDef *uart)
+int32_t uart_disable_rx_th_int(UART_TypeDef *uart)
 {
     if (__RARELY(uart == NULL)) {
         return -1;
     }
+    
+    uart->IER &= 0xFFFFFFFE;
     return 0;
 }
 
-int32_t uart_disable_rxint(UART_TypeDef *uart)
+int32_t uart_enable_rx_err_int(UART_TypeDef *uart)
 {
     if (__RARELY(uart == NULL)) {
         return -1;
     }
+
+    uart->IER |= 0x4;
     return 0;
+}
+
+int32_t uart_disable_rx_err_int(UART_TypeDef *uart)
+{
+    if (__RARELY(uart == NULL)) {
+        return -1;
+    }
+    
+    uart->IER &= 0xFFFFFFFB;
+    return 0;
+}
+
+int32_t uart_get_int_status(UART_TypeDef *uart)
+{
+
+    if (__RARELY(uart == NULL)) {
+        return -1;
+    }
+
+    return uart->IIR;
 }
